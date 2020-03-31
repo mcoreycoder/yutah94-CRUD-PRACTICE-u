09ad68fda43008c1aa4840786acfc7f1f4874470
Michael Corey <michaelcorey@Michaels-MacBook-Pro.local>
2020-03-31 17:00:56 -0600

    Updates

---
+++ precache-manifest.f7e79fdbcd5d91540c2572effa6ac478.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d633b8a6b67f1ae0f1cd7468f93b9314",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5600d237e5986472737f",
+    "url": "/static/css/main.5bbcb8ab.chunk.css"
+  },
+  {
+    "revision": "e2c718fac7fee7444cab",
+    "url": "/static/js/2.4d222b94.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.4d222b94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5600d237e5986472737f",
+    "url": "/static/js/main.04d23101.chunk.js"
+  },
+  {
+    "revision": "7a446f490983632d33a8",
+    "url": "/static/js/runtime-main.74c5b356.js"
+  }
+]);
